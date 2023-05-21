1d09ed07e15e77a3d4f2df2fad0f3f2da0e68074
Patrick Silveira <132209596+PatrickSilveira18@users.noreply.github.com>
2023-05-21 15:31:19 -0300

    Criação dos arquivos
    Criei os arquivos necessários até o momento para o desenvolvimento do projeto e comecei um pequeno esboço

---
+++ ProjetoWeb/dados.php
@@ -0,0 +1,19 @@
+<?php 
+    $produtos = [
+        [
+            "id" => 1,
+            "nome" => 
+            "imagem" =>
+            "preco" =>
+            "descricao" =>
+        ],
+        [
+            "id" => 2,
+            "nome" => 
+            "imagem" =>
+            "preco" =>
+            "descricao" => 
+        ],
+
+    ]
+?>
--- ProjetoWeb/index.php
+++ ProjetoWeb/index.php
@@ -4,9 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Nomedaloja</title>
 </head>
 <body>
-    
+    <div>
+        <div>
+            //Logodaloja
+            <h2>Nomedaloja</h2>
+        </div>
+        <form>
+            <input type="text" required>
+            <button name="pesquisar">Aqui fica aquela lupa do pesquisar</button>
+        <form>
+        
+    </div>
 </body>
 </html>
