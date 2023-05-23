b3c42cdbd9841985dc092db3545d38a9b7f2ccad
Kaulima01 <kaualima6273@gmail.com>
2023-05-22 21:16:15 -0300

    Update dados.php

--- ProjetoWeb/dados.php
+++ ProjetoWeb/dados.php
@@ -2,18 +2,45 @@
     $produtos = [
         [
             "id" => 1,
-            "nome" => 
-            "imagem" =>
-            "preco" =>
-            "descricao" =>
+            "nome" => "Samsung Galaxy A54", 
+            "imagem" =>,
+            "preco" => "R$ 2.179",
+            "descricao" =>,
         ],
         [
             "id" => 2,
-            "nome" => 
-            "imagem" =>
-            "preco" =>
-            "descricao" => 
+            "nome" => "Smart TV LG Ai Thinq", 
+            "imagem" =>,
+            "preco" => "R$ 2.239",
+            "descricao" =>, 
         ],
-
+        [
+            "id" => 3,
+            "nome" => "Monitor Gamer LG 22mp410-b", 
+            "imagem" =>,
+            "preco" => "R$ 565",
+            "descricao" =>, 
+        ],
+        [
+            "id" => 4,
+            "nome" => "Notebook Acer Aspire 3 A314-35 pure silver 14", 
+            "imagem" =>,
+            "preco" => "R$ 1.789",
+            "descricao" =>, 
+        ],
+        [
+            "id" => 5,
+            "nome" => "Micro-ondas Philco PMO23EB", 
+            "imagem" =>,
+            "preco" => "R$ 649",
+            "descricao" =>, 
+        ],
+        [
+            "id" => 6,
+            "nome" => "Tablet Samsung Galaxy Tab A A8 SM-X200 10.5", 
+            "imagem" =>,
+            "preco" => "R$ 1.379",
+            "descricao" =>, 
+        ],
     ]
 ?>
