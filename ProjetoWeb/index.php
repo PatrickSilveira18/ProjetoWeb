63e13a46a3004304fbfcc4c9ca17214e3510240b
André Felipe <91513014+LynnerL@users.noreply.github.com>
2023-05-23 19:54:32 -0300

    Arquivo index corrigido

--- ProjetoWeb/index.php
+++ ProjetoWeb/index.php
@@ -4,19 +4,64 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nomedaloja</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@800&display=swap" rel="stylesheet">
+    <title>Página inicial | Souls Store</title>
 </head>
 <body>
-    <div>
-        <div>
-            //Logodaloja
-            <h2>Nomedaloja</h2>
+    <header>
+        <div class="logo">
+        <a href="index.html" >
+                <img src="img/logo.png" alt="Logo Souls Store">
+        </a>
         </div>
-        <form>
-            <input type="text" required>
-            <button name="pesquisar">Aqui fica aquela lupa do pesquisar</button>
-        <form>
-        
-    </div>
+        <div class="busca">
+            <label>
+                <input type="text" name="busca" placeholder=" Buscar produtos, marcas e muito mais...">
+            </label>
+            <button><img src="img/lupa.png" alt="Buscar"></button>
+        </div>
+        <div class="login">
+            <button  class="botao"><a href="">Cadastro</a></button>
+            <button  class="botao"><a href="">Entrar</a></button>
+        </div>
+        <div class="menu">
+        <nav>
+            <a href="">Categorias</a>
+            <a href="">Ofertas do dia</a>
+            <a href="">Histórico</a>
+            <a href="">Moda</a>
+            <a href="">Vender</a>
+            <a href="">Contato</a>
+        </nav>
+        </div>
+    </header>
+
+    <section>
+            <div class="produto">
+                <img src="img/produto/1_produto_blusa.png" alt="Produto - Camisa Vasco Masculina Building Licenciada Casual">
+                <p>R$112,71</p>
+                <p>FRETE GRÁTIS</p>
+            </div>
+
+            <div class="produto">
+                <img src="img/produto/2_produto_playstation2.png" alt="Produto - Camisa Vasco Masculina Building Licenciada Casual">
+                <p>R$112,71</p>
+                <p>FRETE GRÁTIS</p>
+            </div>
+
+            <div class="produto">
+
+            </div>
+
+            <div class="produto">
+                
+            </div>
+    </section>
+    <footer>
+
+    </footer>
 </body>
 </html>
