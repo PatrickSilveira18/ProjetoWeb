47db5ffac7f27dbd18fa2c94c6f4ad8e4f110195
Kaulima01 <kaualima6273@gmail.com>
2023-05-23 21:41:55 -0300

    Update dados.php

--- ProjetoWeb/dados.php
+++ ProjetoWeb/dados.php
@@ -3,86 +3,141 @@
         [
             "id" => 1,
             "nome" => "Samsung Galaxy A54", 
-            "imagem" => "https://http2.mlstatic.com/D_Q_NP_675865-MLU69483002166_052023-R.webp",
+            "imagem" => "",
             "preco" => "R$ 2.179",
-            "descricao" => "",
+            "descricao" => "Compatível com redes 5G.\n
+            Tela Super AMOLED de 6.4 polegadas.\n
+            Tem 3 câmeras traseiras de 50 MP / 12 MP / 5 MP.\n
+            Câmeras frontais de 32 MP.\n
+            Bateria de 5000mAh.\n
+            Memória interna de 256GB.\n
+            Resistente à água.\n
+            Com sensor de impressão digital.\n
+            Resistente à poeira.\n",
         ],
         [
             "id" => 2,
             "nome" => "Smart TV LG Ai Thinq", 
-            "imagem" => "https://http2.mlstatic.com/D_Q_NP_918680-MLA51853003218_102022-R.webp",
-            "preco" => "R$ 2.239",
-            "descricao" => "", 
+            "imagem" => "",
+            "preco" => "R$ 2.229",
+            "descricao" => "4K.\n
+            Tecnologia HDR para melhor qualidade de imagem.\n
+            Gerencie-o por comando de voz.\n
+            Tem função de compartilhamento de tela.\n
+            Com Apple airplay.\n
+            Alexa e Google Assistant incorporados.\n
+            Sistema operacional webOS 6.0.\n
+            3 portas HDMI e 2 portas USB.\n
+            Dimensões: 112cm de largura, 65.1cm de altura, 5.7cm de profundidade.\n", 
         ],
         [
             "id" => 3,
             "nome" => "Monitor Gamer LG 22mp410-b", 
-            "imagem" => "https://http2.mlstatic.com/D_Q_NP_611126-MLA52286550419_112022-R.webp",
+            "imagem" => "",
             "preco" => "R$ 565",
-            "descricao" => "", 
+            "descricao" => "3D.
+            Tela anti-reflexo.\n
+            Resolução de 1920px-1080px.\n
+            Proporção da tela de 16:9.\n
+            Conexão HDMI.\n", 
         ],
         [
             "id" => 4,
             "nome" => "Notebook Acer Aspire 3", 
-            "imagem" => "https://http2.mlstatic.com/D_Q_NP_629745-MLA50480761415_062022-R.webp",
+            "imagem" => "",
             "preco" => "R$ 1.789",
-            "descricao" => "", 
+            "descricao" => "Processador Intel Celeron.\n
+            Memória RAM de 4GB.\n
+            Tela LED de 14.\n
+            Resolução de 1920x1080px.\n
+            Placa de vídeo Intel UHD Graphics.\n
+            Possui 3 portas USB e porta HDMI.\n", 
         ],
         [
             "id" => 5,
             "nome" => "Micro-ondas Philco PMO23EB", 
-            "imagem" => "https://http2.mlstatic.com/D_Q_NP_921437-MLA52179935853_102022-R.webp",
+            "imagem" => "",
             "preco" => "R$ 649",
-            "descricao" => "", 
+            "descricao" => "Potência de 1100w.\nn
+            Display digital.\n
+            Dimensões: 45cm de largura, 26.2cm de altura, 35.3cm de profundidade.\n
+            Possui bloqueio de segurança.\n
+            Com descongelamento automático por peso.\n
+            Modo rápido de descongelamento por tempo.\n
+            Sua eficiência energética é de A.\n", 
         ],
         [
             "id" => 6,
             "nome" => "Tablet Samsung Galaxy Tab A A8", 
-            "imagem" => "https://http2.mlstatic.com/D_Q_NP_611601-MLU69483886342_052023-R.webp",
+            "imagem" => "",
             "preco" => "R$ 1.379",
-            "descricao" => "", 
+            "descricao" => "Com processador Octa-Core de 2GHz.\n
+            Resolução da tela de 1920x1200px.\n
+            Com leitor micro-SD.\n
+            Memória interna expansível até 1 TB com fonte externa.\n
+            Pesa apenas 508g.\n
+            Com entrada para fone de ouvido.\n", 
         ],
         [
             "id" => 7,
-            "nome" => "", 
+            "nome" => "Camisa Vasco Da Gama", 
             "imagem" => "",
-            "preco" => "",
-            "descricao" => "", 
+            "preco" => "R$ 199,99",
+            "descricao" => "Na cor Bege.\n
+            Ano 2023.\n
+            Versão de torcedor.\n
+            III Uniforme.\n", 
         ],
         [
             "id" => 8,
-            "nome" => "", 
+            "nome" => "Bicicleta Aro 29 Gts Alumínio", 
             "imagem" => "",
-            "preco" => "",
-            "descricao" => "", 
+            "preco" => "R$ 890,10",
+            "descricao" => "Aro 29.\n
+            24 marchas.\n
+            Quadro feito de alumínio,\n
+            Suporta até 120 kg.\n", 
         ],
         [
             "id" => 9,
-            "nome" => "", 
+            "nome" => "Fritadeira Elétrica Air Fryer Gaabor", 
             "imagem" => "",
-            "preco" => "",
-            "descricao" => "", 
+            "preco" => "R$ 289",
+            "descricao" => "Potência: 1200W.\n
+            Contém 1 cesta removível.\n
+            Possui temperatura ajustável entre 80 °c e 200 °c.\n
+            Com temporizador programável.\n
+            Superfície antiaderente.\n", 
         ],
         [
             "id" => 10,
-            "nome" => "", 
+            "nome" => "Purificador Ibbl E-due Equilibrium", 
             "imagem" => "",
-            "preco" => "",
-            "descricao" => "", 
+            "preco" => "R$ 519,90",
+            "descricao" => "Refrigeração Eletrônica.\n
+            Eficiência Bacteriológica.\n", 
         ],
         [
             "id" => 11,
-            "nome" => "", 
+            "nome" => "Impressora Epson EcoTank L3250", 
             "imagem" => "",
-            "preco" => "",
-            "descricao" => "", 
+            "preco" => "R$ 1.117",
+            "descricao" => "Funcionamento por controle de voz.\n
+            Possibilidade de controlar a partir de dispositivos movéis\n
+            Incluso cabo USB\n
+            Consumo elétrico de 20W em uso e 0,7W em repouso\n", 
         ],
         [
             "id" => 12,
-            "nome" => "", 
+            "nome" => "Câmera de segurança Intelbras iM3", 
             "imagem" => "",
-            "preco" => "",
-            "descricao" => "", 
+            "preco" => "R$ 279,90",
+            "descricao" => "Definição da câmera: 2MP.\n
+            Capacidade de armazenamento de 256GB.\n
+            Áudio bidirecional.\n
+            Inclui visão noturna.\n
+            Peso: 102.7g.\n
+            Possui sensor de movimento.\n", 
         ],
 
     ]
